eaab2e60398ba3e238675198279048a60957579b
Michel Adrion <micheladrion@hotmail.com>
2015-06-27 08:16:57 +0300

    init

--- www/barcode.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    scan: function ( successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Barcode", "scan", [] );
-    }
-};
